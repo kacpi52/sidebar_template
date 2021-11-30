39628a1d463eedbc8499f14983c074edd9b783a7
kacpi52 <kacperkrajewski52@gmail.com>
2021-11-30 19:59:39 +0100

    hidden button

--- src/Components/TopList/TopList.jsx
+++ src/Components/TopList/TopList.jsx
@@ -1,4 +1,4 @@
-import React , {useState} from 'react';
+import React , {useState, useEffect} from 'react';
 import './TopList.scss';
 import {Container, Row} from 'react-bootstrap';
 import SingleElem from './SingleElem';
@@ -6,17 +6,45 @@ import { titleArray } from './TitleArray';
 
 const TopList = () => {
     let displayArray = [];
-    const [listSize , setListSize] = useState(0); 
+    const [listSize , setListSize] = useState(0),
+    [buttonVisibility, setButtonVisibility] = useState(true);
+
+    
     const extendListSize = () => {
         setListSize(listSize+10);
-    };
+    }
     for (let i = 0; i < listSize; i++) {
         displayArray.push(<SingleElem movieTitle={titleArray[i]}/>);
     }
+    const scrollListener = () => {
+        let visibilityDistance = 0;
+        if(listSize > 11 && listSize <21){
+            visibilityDistance = 2000;
+        } else if (listSize > 20 ){
+            visibilityDistance = 3000;                   // tu narazie te wartosci sa na sztywno 
+        } else {
+            visibilityDistance = 1000;
+        }
+
+        const distFromTop = document.body.scrollTop ||
+            document.documentElement.scrollTop;;
+        if (distFromTop > visibilityDistance) {
+            setButtonVisibility(false);
+            console.log(visibilityDistance);
+        } else {
+            setButtonVisibility(true)
+        }
+    }
+    useEffect(() => {
+            window.addEventListener("scroll", scrollListener);
+        return () => {
+            window.removeEventListener("scroll", scrollListener);
+        }
+    }, [])
 
     return (
         <div className='List'>
-            <button className='List__button' onClick={extendListSize}>{listSize>0 ? 'SHOW MORE' : 'TOP 10'}</button>
+            <button className={buttonVisibility ? 'List__button' : 'List__button--hidden'} onClick={extendListSize}>{listSize>0 ? 'SHOW MORE' : 'TOP 10'}</button>
             <Container>
                 <Row>
                     {displayArray}
