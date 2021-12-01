b4bff345b8a81665764ce67fd1befb2cdf0d13d7
kacpi52 <kacperkrajewski52@gmail.com>
2021-12-01 12:45:16 +0100

    rb

--- src/Components/TopList/TopList.jsx
+++ src/Components/TopList/TopList.jsx
@@ -5,32 +5,19 @@ import SingleElem from './SingleElem';
 import { titleArray } from './TitleArray';
 
 const TopList = () => {
-    let displayArray = [];
-    const [listSize , setListSize] = useState(0),
-    [buttonVisibility, setButtonVisibility] = useState(true);
-
     
-    const extendListSize = () => {
-        setListSize(listSize+10);
-    }
+    const  [listSize, setListSize] = useState(0),
+    [buttonVisibility, setButtonVisibility] = useState(true);
+    let displayArray = [];
+    
     for (let i = 0; i < listSize; i++) {
         displayArray.push(<SingleElem movieTitle={titleArray[i]}/>);
     }
     const scrollListener = () => {
-        let visibilityDistance = 0;
-        if(listSize > 11 && listSize <21){
-            visibilityDistance = 2000;
-        } else if (listSize > 20 ){
-            visibilityDistance = 3000;                   // tu narazie te wartosci sa na sztywno 
-        } else {
-            visibilityDistance = 1000;
-        }
-
-        const distFromTop = document.body.scrollTop ||
+         const distFromTop = document.body.scrollTop ||
             document.documentElement.scrollTop;;
-        if (distFromTop > visibilityDistance) {
+        if (distFromTop > 3500) {                                       // wartosc dana na sztywno
             setButtonVisibility(false);
-            console.log(visibilityDistance);
         } else {
             setButtonVisibility(true)
         }
@@ -44,7 +31,12 @@ const TopList = () => {
 
     return (
         <div className='List'>
-            <button className={buttonVisibility ? 'List__button' : 'List__button--hidden'} onClick={extendListSize}>{listSize>0 ? 'SHOW MORE' : 'TOP 10'}</button>
+            <button className={buttonVisibility ? 'List__button' : 'List__button--hidden'}
+                onClick={() => {
+                     setListSize(listSize + 10)
+                }}>
+                 {listSize>0 ? 'SHOW MORE' : 'TOP 10'}
+                 </button>
             <Container>
                 <Row>
                     {displayArray}
