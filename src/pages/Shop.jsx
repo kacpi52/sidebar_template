6ce5ad4994994db6f92e3a702175720d743c9650
kacpi52 <kacperkrajewski52@gmail.com>
2021-12-17 15:56:11 +0100

    xml parse

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
-import Menu from '../Components/SidebarMenu/Menu';
-import XMLToReact from 'xml-to-react';
-import '../Data/Opony.xml';
+import React, { useState, useEffect } from "react";
+import Menu from "../Components/SidebarMenu/Menu";
 
 const Shop = () => {
-    
-    return (
-        <>
-        <Menu/>
-            <p1>Shop</p1>
-            
-        </>
-    )
-}
+  const [xmlElems, setXmlElems] = useState([]);
 
-export default Shop
+  useEffect(() => {
+    fetch("/Data/Opony.xml")
+      .then((res) => res.text())
+      .then((xmlString) => {
+        const xmlDomObj = new window.DOMParser().parseFromString(
+          xmlString,
+          "text/xml"
+        );
+        const xmlOElems = xmlDomObj.getElementsByTagName("o"); 
+        const xmlElemsArr = Array.from(xmlOElems).map((elem, i) => {
+          return {
+            url: elem.getAttribute("url"),
+            price: elem.getAttribute("price"),
+            weight: elem.getAttribute("weight"),
+            img: elem.getAttribute("main")
+          };
+        });
+        setXmlElems(xmlElemsArr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <Menu />
+      <p>Shop</p>
+      {xmlElems.map((elem, i) => (
+        <h4 key={i}>{elem.url}</h4>
+      ))}
+      <p>{xmlElems[2].price}</p>
+    </>
+  );
+};
+
+export default Shop;
