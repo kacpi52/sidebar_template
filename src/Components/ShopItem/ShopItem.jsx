74bbcc834da16b06decf6bc2e7e74d9ce16d156c
kacpi52 <kacperkrajewski52@gmail.com>
2021-12-22 15:57:59 +0100

    xml data build

---
+++ src/Components/ShopItem/ShopItem.jsx
@@ -0,0 +1,18 @@
+import React  from 'react';
+import './ShopItem.scss';
+import {Col, Image} from 'react-bootstrap';
+const ShopItem = ({title, prod, imgurl, price}) => {
+    
+    return (
+        <Col xs={12} md={6} lg={4} >
+            <div className="item">
+                <p>{prod}</p>
+                <Image rounded src={imgurl} className='item__image' />
+                <p>{title}</p>
+                <p>{price} zł/sztuka </p>
+            </div>
+        </Col>
+    )
+}
+
+export default ShopItem;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Menu from "../Components/SidebarMenu/Menu";
+import ShopItem from "../Components/ShopItem/ShopItem";
+import {Row, Col, Container} from 'react-bootstrap';
 
 const Shop = () => {
+  const itemsArray=[];
   const [xmlElems, setXmlElems] = useState([]);
-
+  
   useEffect(() => {
     fetch("/Data/Opony.xml")
       .then((res) => res.text())
@@ -12,35 +15,45 @@ const Shop = () => {
           xmlString,
           "text/xml"
         );
-        const xmlOElems = xmlDomObj.getElementsByTagName("o"); 
+        const xmlOElems = xmlDomObj.getElementsByTagName("o");
         const xmlElemsArr = Array.from(xmlOElems).map((elem, i) => {
           return {
-            url: elem.getAttribute("url"),
             price: elem.getAttribute("price"),
-            weight: elem.getAttribute("weight"),
-            imgurl: elem.getElementsByTagName("imgs"),
-            attr: elem.getAttribute("attrs").getElementsByName("Producent")
+            desc: elem.querySelector("desc").textContent,
+            imgurl: elem.querySelector("imgs main").getAttribute('url'),
+            producent: elem.querySelector("attrs a[name='Producent']").textContent
           };
         });
         setXmlElems(xmlElemsArr);
-        console.log(xmlElemsArr[0].imgurl)
+        console.log(xmlElemsArr[0].imgurl);
+        console.log(xmlElemsArr[1].imgurl);
+        console.dir(xmlElemsArr[0].attr)
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  for (let i = 0; i < 7; i++) {
+    itemsArray.push(<ShopItem
+      title={xmlElems.length > 0 && xmlElems[i].desc}
+      prod={xmlElems.length > 0 && xmlElems[i].producent}
+      imgurl={xmlElems.length > 0 && xmlElems[i].imgurl}
+      price={xmlElems.length > 0 && xmlElems[i].price}
+    />);
+  }
+
   return (
     <>
       <Menu />
-      <p>Shop</p>
-      {xmlElems.map((elem, i) => (
-        <h4 key={i}>{elem.url}</h4>
-      ))}
-      {xmlElems.map((elem, i) => (
-        <h4 key={i}>{elem.attr}</h4>
-      ))}
-    
+      <Container>
+        <Row>
+        <Col xs={12}>LESS TRACTION MORE ACTION</Col>
+        </Row>
+        <Row>
+          {itemsArray}
+        </Row>
+      </Container>
       
     </>
   );
