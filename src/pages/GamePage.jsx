82eea5aea76af21e14e3040dca1c61f0f807d260
LAPTOP-4H23UC8R\kacpe <kacperkrajewski525@gmail.com>
2022-02-25 14:59:37 +0100

    tries_restart

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,86 +1,97 @@
-import React, {useState} from 'react';
-import Menu from '../Components/SidebarMenu/Menu';
-import { Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from "react";
+import Menu from "../Components/SidebarMenu/Menu";
+import { Container, Row, Col } from "react-bootstrap";
 
 const numbersQuantity = 20;
-const numbersRange = 20; 
-const ranArr = Array.from({length:numbersQuantity},() => Math.floor(Math.random() * numbersRange));
+const numbersRange = 20;
+const ranArr = Array.from({ length: numbersQuantity }, () =>
+  Math.floor(Math.random() * numbersRange)
+);
 const triesLimit = 3;
 
 const GamePage = () => {
-    const [userNumber, setUserNumber] = useState(false), 
-          [errorHandler, setErrorHandler]= useState(false),
-          [resultHandler, setResultHandler]= useState(false),
-          [triesCounter, setTriesCounter]= useState(0);
-   
-    const inputHandler = (text) => {
-      setUserNumber(parseInt(text.target.value))
-    };
-    const saveUserNumber = (event) => {
-      setTriesCounter(triesCounter+1);
-      if (isNaN(userNumber)) {
-        setErrorHandler('Podaj cyfre')
-      } else {
+  const [userNumber, setUserNumber] = useState(false),
+    [errorHandler, setErrorHandler] = useState(false),
+    [resultHandler, setResultHandler] = useState(false),
+    [triesCounter, setTriesCounter] = useState(0);
+
+  const inputHandler = (text) => {
+    setUserNumber(parseInt(text.target.value));
+  };
+  const saveUserNumber = (event) => {
+    setTriesCounter(triesCounter + 1);
+    if (isNaN(userNumber)) {
+      setErrorHandler("Podaj cyfre");
+    } else {
       event.preventDefault();
       setErrorHandler(false);
-      if (triesCounter<triesLimit) {
+      if (triesCounter < triesLimit && resultHandler !== true) {
         iterArray(ranArr);
       }
-      }
-      
-    };
-    
-    const iterArray = (array) => {
-      let i = 0;
-      while (array[i-1] !== userNumber && i<numbersRange){
-        i+=1;
-        console.log(i);
-      };
-      if (array[i-1] === userNumber){
-        setResultHandler(true);
-        console.log(`traf na iteracji ${i}`);
-      } else {
-        setResultHandler(false);
-      }
-    };
-    
+    }
+  };
+
+  const iterArray = (array) => {
+    let i = 0;
+    while (array[i - 1] !== userNumber && i < numbersRange) {
+      i += 1;
+      console.log(i);
+    }
+    if (array[i - 1] === userNumber) {
+      setResultHandler(true);
+      console.log(`traf na iteracji ${i}`);
+    } else {
+      setResultHandler(false);
+    }
+  };
+  const resetAll = () => {
+    setResultHandler(false);
+    setTriesCounter(0);
+  };
+
   return (
-      <>
-          <Menu /> 
-          <Container>
-            <Row>
-              <Col>
-                <p>Game of Numbers</p>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <input type='text' onChange={inputHandler}  />
-                <button type='submit' onClick={saveUserNumber}>PLAY</button>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <p>Twoja liczba to {userNumber}</p>
-                {errorHandler}
-                <p>Wynik gry: {resultHandler?  'Trafiles':'Graj dalej'}</p>
-                <p>{triesCounter<triesLimit ? `Zostaly ci jeszcze ${triesLimit-triesCounter} próby` : `Wykorzystałeś limit prób`}</p>
-              </Col>
-              <Col>
-                <p>Dlugosc tablicy to {ranArr.length}</p>
-                <p>{ranArr.join(' , ')}</p>
-              </Col>
-            </Row>
-            
-          </Container>  
-      </>
-  )
+    <>
+      <Menu />
+      <Container>
+        <Row>
+          <Col>
+            <p>Game of Numbers</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <input type="text" onChange={inputHandler} />
+            <button type="submit" onClick={saveUserNumber}>
+              PLAY
+            </button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p>Twoja liczba to {userNumber}</p>
+            {errorHandler}
+            <p>Wynik gry: {resultHandler ? "Trafiles" : "Graj dalej"}</p>
+            <p> {resultHandler && "Zresetuj aby zagrać na nowo"}</p>
+            <p>
+              {triesCounter < triesLimit
+                ? `Zostaly ci jeszcze ${triesLimit - triesCounter} próby`
+                : `Wykorzystałeś limit prób`}
+            </p>
+          </Col>
+          <Col>
+            <button type="submiy" onClick={resetAll}>
+              RESTART
+            </button>
+            <p>Dlugosc tablicy to {ranArr.length}</p>
+            <p>{ranArr.join(" , ")}</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default GamePage;
-
-
-
 
 /*
 const keyHolder = (event) =>{
